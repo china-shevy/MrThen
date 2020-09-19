39ca19bcee506525c30aab2a604ae73a2774847c
高伟 <gaowei@gaoweideMBP.lan>
2020-09-19 20:00:45 +0800

    add is_leap_year

--- src/app.php
+++ src/app.php
@@ -1,6 +1,16 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
+
+function is_leap_year($year = null) {
+    if (null === $year) {
+        $year = idate('Y');
+    }
+
+    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
+
 
 $routes = new Routing\RouteCollection();
 
@@ -16,6 +26,18 @@ $routes->add('hello', new Routing\Route('/hello/{name}', [
         return $response;
     }
 ]));
+
 $routes->add('bye', new Routing\Route('/bye'));
+
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
+    'year' => null,
+    '_controller' => function ($request) {
+        if (is_leap_year($request->attributes->get('year'))) {
+            return new Response('Yep, this is a leap year');
+        }
+        
+        return new Response('Nope, this is not a leap year');
+    }
+]));
 
 return $routes;
