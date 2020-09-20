903d6e982115e05638f453ae30fd47f08f414e41
高伟 <gaowei@gaoweideMBP.lan>
2020-09-20 20:41:33 +0800

    增加测试

--- tests/Simplex/Tests/FrameworkTest.php
+++ tests/Simplex/Tests/FrameworkTest.php
@@ -5,6 +5,9 @@ namespace Simplex\Tests;
 use PHPUnit\Framework\TestCase;
 use Simplex\Framework;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 class FrameworkTest extends TestCase
@@ -13,7 +16,7 @@ class FrameworkTest extends TestCase
     protected $controllerResolver;
     protected $argumentResolver;
 
-    public function testNotFoundHanding()
+    /* public function testNotFoundHanding()
     {
         $framework = $this->getFrameworkForException(new ResourceNotFoundException());
 
@@ -46,5 +49,33 @@ class FrameworkTest extends TestCase
         $response = $framework->handle(new Request());
 
         $this->assertEquals(500, $response->getStatusCode());
+    } */
+
+    public function testControllerResponse()
+    {
+        $matcher = $this->createMock('Symfony\Component\Routing\Matcher\UrlMatcherInterface');
+        $matcher
+        ->expects($this->once())
+        ->method('match')
+        ->will($this->returnValue(array(
+            '_route' => 'foo',
+            'name' => 'Fabien',
+            '_controller' => function ($name) {
+                return new Response('Hello ' . $name);
+            }
+        )));
+        $matcher
+        ->expects($this->once())
+        ->method('getContext')
+        ->will($this->returnValue($this->createMock('Symfony\Component\Routing\RequestContext')));
+        $controllerResolver = new ControllerResolver();
+        $argumentResolver = new ArgumentResolver();
+
+        $framework = new Framework($matcher, $controllerResolver, $argumentResolver);
+
+        $response = $framework->handle(new Request());
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertContains('Hello Fabien', $response->getContent());
     }
 }
